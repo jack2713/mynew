e7f6bf9e8e0625d6f7ced42d1729678b89e76419
jack2713 <jack2.fan@outlook.com>
2026-02-14 16:15:45 +0800

    Update my2.py

--- TMP/my2.py
+++ TMP/my2.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 # 全局排除关键词定义
-EXCLUDE_KEYWORDS = ["成人", "激情", "虎牙", "体育", "熊猫", "提示","记录","解说","春晚","直播中国","更新","赛事","SPORTS","电视剧","优质个源","明星","主题片","戏曲","游戏","MTV","收音机","悍刀","家人","甄嬛"]
+EXCLUDE_KEYWORDS = ["成人", "激情", "虎牙", "体育", "熊猫", "提示","记录","解说","春晚","直播","更新","赛事","SPORTS","电视剧","优质个源","明星","主题片","戏曲","游戏","MTV","收音机","悍刀","家人","甄嬛"]
 
 class TVSourceProcessor:
     def __init__(self):
