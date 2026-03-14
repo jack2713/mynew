24d371ae4955b20dd2d247dea2cb0a8053ac6ab4
jack2713 <jack2.fan@outlook.com>
2026-03-14 19:51:51 +0800

    Update ttest.py

--- TMP/ttest.py
+++ TMP/ttest.py
@@ -128,8 +128,8 @@ class TVSourceProcessor:
         # 只使用指定的URL
         urls = [
         #"https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/Jsnzkpg/Jsnzkpg1",
-        "https://raw.githubusercontent.com/fafa002/yf2025/refs/heads/main/yiyifafa.txt",
-        #"https://raw.githubusercontent.com/zxmlxw520/5566/refs/heads/main/cjdszb.txt",
+        #"https://raw.githubusercontent.com/fafa002/yf2025/refs/heads/main/yiyifafa.txt",
+        "https://raw.githubusercontent.com/zxmlxw520/5566/refs/heads/main/cjdszb.txt",
         ]
         print(f"源URL: {len(urls)}个")
         
